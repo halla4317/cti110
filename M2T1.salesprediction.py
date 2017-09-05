f088be217bed2acb9466cf8713e2aea290940ba7
halla4317 <31639254+halla4317@users.noreply.github.com>
2017-09-04 22:23:25 -0400

    Add files via upload
    First Upload (Begin)

---
+++ M2T1.salesprediction.py
@@ -0,0 +1,15 @@
+# cti 110
+# m2t1- sales prediction
+# 8/30
+# Hall, A
+
+# suggested variables: totalsales, annual profit
+
+# ask the user for total sales
+totalsales = int (input('Type totalsales'))
+
+# calculate the profit amount
+annualprofit = 0.23 * totalsales
+
+# print the profit amount
+print ('annualprofit is', annualprofit)
