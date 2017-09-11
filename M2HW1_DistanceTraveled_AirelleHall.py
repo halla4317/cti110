fa0b30dc4545c7edec62cbd38ac44b3631ca90c8
halla4317 <31639254+halla4317@users.noreply.github.com>
2017-09-10 20:07:17 -0400

    Add files via upload
    Distance Traveled

---
+++ M2HW1_DistanceTraveled_AirelleHall.py
@@ -0,0 +1,13 @@
+# CTI-110
+# M2HW1-Distance Traveled
+# Airelle Hall
+# 5 Sept 2017
+
+distance=70*6
+print ('1. The distance that the car will travel in 6 hours is', distance, 'miles' )
+
+distance=70*10 
+print ('2. The distance that the car will travel in 10 hours is', distance, 'miles' )
+
+distance=70*15
+print ('3. The distance that the car will travel in 15 hours is', distance, 'miles' )
